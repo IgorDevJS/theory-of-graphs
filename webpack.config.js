a07c93dceac1793186c6aa7e7444170a1a8988f7
Igor Gonçalves <igor_goncalves_ferrari@hotmail.com>
2018-04-26 14:55:31 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,20 @@ module.exports = {
     output: {
         filename: "bundle.js"
     },
+    devServer: {
+        inline: true,
+        contentBase: './dist',
+        port: 3333
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015']
+            }
+        }]
+    },
     mode: "development"
 }
