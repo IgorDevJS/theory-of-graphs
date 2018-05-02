826279c1e175bb5315ca6166529930487cc05038
Igor Gonçalves <igor_goncalves_ferrari@hotmail.com>
2018-05-02 13:55:22 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,34 @@
 module.exports = {
-    entry: "./entry.js",
-    output: {
-        filename: "bundle.js"
+  entry: './entry.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  devServer: {
+    inline: true,
+    contentBase: './dist',
+    port: 3333,
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015'],
+      },
     },
-    devServer: {
-        inline: true,
-        contentBase: './dist',
-        port: 3333
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015']
-            }
-        }]
-    },
-    mode: "development"
-}
+    {
+      test: /\.scss$/,
+      use: [
+        {
+          loader: 'style-loader', // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS
+        }, {
+          loader: 'sass-loader', // compiles Sass to CSS
+        },
+      ],
+    }],
+  },
+  mode: 'development',
+};
