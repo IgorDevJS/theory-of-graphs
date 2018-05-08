4ae4a8ddb2ddf291a243626e5fe64b4ee6eeb193
Igor Gonçalves <igor_goncalves_ferrari@hotmail.com>
2018-05-08 15:23:07 -0300

    Create GraphContainer component, Implement functionality DragAndDrop

---
+++ src/components/GraphContainer/GraphContainer.js
@@ -0,0 +1,68 @@
+import interact from 'interactjs';
+
+import './GraphContainer.scss';
+import Component from '../Component/Component';
+import Vertice from '../Vertice/Vertice';
+
+export default class GraphContainer extends Component {
+  constructor() {
+    super();
+    this.init();
+    this.configDropZone();
+  }
+
+  init() {
+    this.elem = document.createElement('div');
+    this.elem.classList.add('GraphContainer');
+  }
+
+  configDropZone() {
+    interact(this.elem).dropzone({
+      // only accept elements matching this CSS selector
+      accept: '.Vertice, .Aresta',
+      // Require a 75% element overlap for a drop to be possible
+      overlap: 0.75,
+
+      // listen for drop related events:
+
+      ondropactivate(event) {
+        // add active dropzone feedback
+        event.target.classList.add('drop-active');
+        event.relatedTarget.classList.add('drop-active');
+      },
+      ondragenter(event) {
+        const draggableElement = event.relatedTarget;
+        const dropzoneElement = event.target;
+
+        // feedback the possibility of a drop
+        dropzoneElement.classList.add('drop-target');
+        draggableElement.classList.add('can-drop');
+      },
+      ondragleave(event) {
+        // remove the drop feedback style
+        event.target.classList.remove('drop-target');
+        event.relatedTarget.classList.remove('can-drop');
+      },
+      ondrop: (event) => {
+        const e = event;
+        // case sample
+        if (e.relatedTarget.getAttribute('is-sample')) {
+          e.relatedTarget.style.transform = 'translate(0px, 0px)';
+          e.relatedTarget.setAttribute('data-x', 0);
+          e.relatedTarget.setAttribute('data-y', 0);
+
+          // create new Vertice
+          const v1 = new Vertice();
+          v1.setValue('1');
+          v1.render(this.elem);
+        }
+      },
+      ondropdeactivate(event) {
+        // remove active dropzone feedback
+        event.target.classList.remove('drop-active');
+        event.target.classList.remove('drop-target');
+        event.relatedTarget.classList.remove('drop-active');
+      },
+    });
+  }
+}
--- src/components/Vertice/Vertice.js
+++ src/components/Vertice/Vertice.js
@@ -1,14 +1,17 @@
-import './Vertice.scss';
+import interact from 'interactjs';
 
+import './Vertice.scss';
 import Component from '../Component/Component';
 
 export default class Vertice extends Component {
   // create elem to show in the panel to listener hold event
   // call callback on hold event to create new component
-  static verticeSample(callback) {
+  static verticeSample() {
     const container = document.createElement('div');
     container.classList.add('container-vertice-sample');
     const elem = Vertice.createElem();
+    elem.classList.add('Vertice-Sample');
+    elem.setAttribute('is-sample', 1);
     container.appendChild(elem);
     const p = document.createElement('p');
     p.innerText = 'Vértice';
@@ -20,7 +23,53 @@ export default class Vertice extends Component {
   static createElem() {
     const elem = document.createElement('div');
     elem.classList.add('Vertice');
+    Vertice.configDrag(elem);
     return elem;
+  }
+
+  static configDrag(elem) {
+    // target elements with the "draggable" class
+    interact(elem)
+      .draggable({
+        // enable inertial throwing
+        inertia: false,
+        // keep the element within the area of it's parent
+        restrict: {
+          endOnly: true,
+          elementRect: {
+            top: 0,
+            left: 0,
+            bottom: 1,
+            right: 1,
+          },
+        },
+        // enable autoScroll
+        autoScroll: true,
+
+        // call this function on every dragmove event
+        onmove(event) {
+          const { target } = event;
+          // keep the dragged position in the data-x/data-y attributes
+          const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+          const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+          // translate the element
+          target.style.webkitTransform = `translate(${x}px, ${y}px)`;
+          target.style.transform = `translate(${x}px, ${y}px)`;
+
+          // update the posiion attributes
+          target.setAttribute('data-x', x);
+          target.setAttribute('data-y', y);
+        },
+        // call this function on every dragend event
+        onend(event) {
+          const textEl = event.target.querySelector('p');
+
+          if (textEl) {
+            textEl.textContent = `moved a distance of ${(Math.sqrt(((event.pageX - event.x0) ** 2) + ((event.pageY - event.y0) ** 2))).toFixed(2)}px`;
+          }
+        },
+      });
   }
 
   constructor() {
