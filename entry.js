ebeee1944488b46e512cc41f07c902a8dabd34e3
Igor Gonçalves <igor_goncalves_ferrari@hotmail.com>
2018-05-08 14:07:55 -0300

    Update entry.js

--- entry.js
+++ entry.js
@@ -1,16 +1,9 @@
-import Vertice from './src/components/Vertice/Vertice';
-import ToolPanel from './src/components/ToolPanel/ToolPanel';
+import MainContainer from './src/components/MainContainer/MainContainer';
 
-// create new Vertice
-const v1 = new Vertice();
-v1.setValue('1');
-
-// create new ToolPanel
-const toolPanel = new ToolPanel();
+const mainContainer = new MainContainer();
 
 function init() {
-  v1.render(document.body);
-  toolPanel.render(document.body);
+  mainContainer.render(document.body);
 }
 
 document.addEventListener('DOMContentLoaded', init);
