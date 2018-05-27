5b21498b9e96c5784f5b31052afe64d722d8392b
IgorDevJS <igor_goncalves_ferrari@hotmail.com>
2018-05-27 12:46:13 -0300

    Update GraphContainer.js

--- src/components/GraphContainer/GraphContainer.js
+++ src/components/GraphContainer/GraphContainer.js
@@ -59,8 +59,8 @@ export default class GraphContainer extends Component {
 
           // create new Vertice
           const v1 = new Vertice({ top: rect.top, left: rect.left });
-          this.manageData.pushVerticeData(v1);
           v1.value = '1';
+          this.manageData.pushVerticeData(v1);
           v1.render(this.elem);
         }
       },
